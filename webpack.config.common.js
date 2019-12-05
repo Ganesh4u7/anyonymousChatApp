cba42fbf13b0e91b2a101b8c3e7ccde5d9f3faf8
Ganesh4u7 <42564733+Ganesh4u7@users.noreply.github.com>
2019-12-05 09:47:08 +0530

    Add files via upload

---
+++ webpack.config.common.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        'app': './assets/app/main.ts'
+    },
+
+    resolve: {
+        extensions: ['.js', '.ts']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [{ loader: 'html-loader' }]
+            },
+            {
+                test: /\.css$/,
+                use: [{ loader: 'raw-loader' }]
+            },
+          {
+            test: /\.(png|jpg|gif)$/i,
+            use: [
+              {
+                loader: 'url-loader',
+                options: {
+                  limit: 8192
+                }
+              }
+            ]
+          }
+        ],
+        exprContextCritical: false
+
+    }
+};
